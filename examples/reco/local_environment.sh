9a9471f8970728874106d580678ab779d95dfa4c
Dario Berzano <dario.berzano@cern.ch>
2018-05-08 15:29:27 +0200

    WIP: modifications to our test files

--- examples/reco/local_environment.sh
+++ examples/reco/local_environment.sh
@@ -1,5 +1,5 @@
 ALIBUILD_PREFIX=/home/dberzano/alisw/alibuild
 export ALIBUILD_WORK_DIR=/home/dberzano/alice-ng/sw
-eval $($ALIBUILD_PREFIX/alienv printenv -q AliPhysics/latest-prod-user,AliDPG/latest-prod-user)
+eval $($ALIBUILD_PREFIX/alienv printenv -q AliPhysics/latest-aliroot5-user,AliDPG/latest)
 type aliroot &> /dev/null
 [[ $ALIDPG_ROOT ]] || exit 1
